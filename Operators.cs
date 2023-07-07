7eb2967c60644d965596f2326c7825f1e4f63705
Şamil <sml.ozclk@gmail.com>
2023-07-07 17:23:18 +0300

    Sepearte IntOperator to its own file

--- Operators.cs
+++ Operators.cs
@@ -1,49 +1,5 @@
 ﻿namespace Forge.Models.Operators
 {
-    public struct IntOperator : IOperator<int>
-    {
-        public int Add(int a, int b) => a + b;
-
-        public int Subtract(int a, int b) => a - b;
-
-        public int Mul(int a, int b) => a * b;
-
-        public int Div(int a, int b) => a / b;
-
-        public int Cast(int a) => a;
-
-        public int Cast(long a) => (int) a;
-
-        public int Cast(float a) => (int) a;
-
-        public int Cast(double a) => (int) a;
-
-        public int Cast(decimal a) => (int) a;
-
-        public int ConvertToInt(int a) => a;
-
-        public long ConvertToLong(int a) => a;
-
-        public float ConvertToFloat(int a) => (float)a;
-
-        public double ConvertToDouble(int a) => a;
-
-        public decimal ConvertToDecimal(int a) => a;
-
-        ///LOGICALS
-
-        public bool Equal(int a, int b) => a == b;
-
-        public bool GreaterThan(int a, int b) => a > b;
-
-        public bool GreaterThanEqual(int a, int b) => a >= b;
-
-        public bool LessThan(int a, int b) => a < b;
-
-        public bool LessThanEqual(int a, int b) => a <= b;
-
-        public bool NotEqual(int a, int b) => a != b;
-    }
 
     public struct FloatOperator : IOperator<float>
     {
